2516d738b51848b3df877448435caa620b7ae141
Lucas Evangelista <130062327+lucasevangelistaa@users.noreply.github.com>
2025-03-29 11:48:39 -0300

    Update tmdb.js

--- api/tmdb.js
+++ api/tmdb.js
@@ -6,7 +6,6 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const fetchMovies = async (endpoint) => {
   const response = await fetch(`${BASE_URL}${endpoint}?api_key=${API_KEY}&language=pt-BR`); // Fazendo a requisição para a API
   if (!response.ok) {
-    throw new Error('Failed to fetch movies');
     throw new Error('Falha ao retornar os dados da API');
   }
   return response.json();
